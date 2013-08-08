a437ab4b1ffe013ce37af2cd981dc54ae8e46c9c
Kyle Robinson Young <kyle@dontkry.com>
2013-08-07 22:07:28 -0700

    initial

---
+++ README.md
@@ -0,0 +1,28 @@
+# voxel-settings
+
+voxel.js config for [cettings](https://github.com/shama/cettings).
+
+## example
+
+[View this example](http://shama.github.io/cettings)
+
+```js
+var settings = require('cettings')(require('voxel-settings')(game))
+```
+
+# install
+
+With [npm](https://npmjs.org) do:
+
+```
+npm install voxel-settings
+```
+
+Use [browserify](http://browserify.org) to `require('voxel-settings')`.
+
+## release history
+* 0.1.0 - initial release
+
+## license
+Copyright (c) 2013 Kyle Robinson Young<br/>
+Licensed under the MIT license.
---
+++ index.js
@@ -0,0 +1,39 @@
+module.exports = function(game) {
+  function bindkey(key, val) {
+    key = key.split('.')
+    if (key[0] === 'controls') {
+      Object.keys(game.keybindings).forEach(function(k, i) {
+        var v = game.keybindings[k]
+        if (v === key[1]) game.keybindings[k] = null
+      })
+      game.keybindings[val] = key[1]
+    }
+  }
+
+  var config = {
+    controls: {},
+    textures: {
+      texturePath: {
+        value: './textures/',
+        label: 'texture path',
+      },
+    },
+  }
+
+  var controls = [
+    'forward', 'W',
+    'backward', 'S',
+    'left', 'A',
+    'right', 'D',
+    'fire', '<mouse 1>',
+  ]
+  for (var i = 0; i < controls.length; i += 2) {
+    config.controls[controls[i]] = {
+      value: controls[i+1],
+      type: 'bindkey',
+      on: bindkey,
+    }
+  }
+
+  return config
+}
